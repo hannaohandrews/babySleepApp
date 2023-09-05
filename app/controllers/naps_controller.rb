58b6887ea84d4e68dfafb80be70ec5f51ce31727
Hanna Oh Andrews <63070798+hannaohandrews@users.noreply.github.com>
2023-09-04 21:46:02 -0700

    added /naps/new

--- app/controllers/naps_controller.rb
+++ app/controllers/naps_controller.rb
@@ -6,4 +6,18 @@ class NapsController < ApplicationController
   def show
     @nap = Nap.find(params[:id])
   end
+
+  def new
+    @nap = Nap.new
+  end
+
+  def create
+    @nap = Nap.new(nap_params)
+
+    if @nap.save
+      redirect_to @nap
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
